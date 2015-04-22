ca44e4db896c702d697e00e327c133d72658cf4a
yunchiri <hernardo@gmail.com>
2015-04-22 15:56:04 +0900

    fix hangul

--- main.js
+++ main.js
@@ -15,22 +15,46 @@ var testHtml = fs.readFileSync('TargetList.html','utf8');
 var url = 'http://www.soccerline.co.kr/slboard/list.php?code=soccerboard'
 
 
-  request(url, function(err, resp, body) {
-     if (err)   throw err;
-    var bodyWithCorrectEncoding = iconv.decode(body, 'iso-8859-1');
- console.log(bodyWithCorrectEncoding);
+var requestOptions  = { encoding: null, uri: url};
 
-//$ = cheerio.load(body);
+request(requestOptions, function(error, response, body) {
+    var utf8String = iconv.decode(body, 'euc-kr');
+    //console.log(utf8String);
 
+
+$ = cheerio.load(utf8String);
 //console.log($.html());
 //console.log($('.te1').html());
 //console.log($('#main_soccer').find('a');
 //console.log($('#main_soccer').find('a').attr('href'));
 
 //
+$('#main_soccer td a').each(function(i, elem) {
+  console.log($(this).text());
+  console.log($(this).attr('href'));
+});
+
+
+});
+
+
+
+//   request(url, function(err, resp, body) {
+//      if (err)   throw err;
+//     //var bodyWithCorrectEncoding = iconv.decode(body, 'utf-8');
+//  // console.log(bodyWithCorrectEncoding);
+
+// $ = cheerio.load(body);
+// $ = cheerio.load(body.toString('utf8'));
+// //console.log($.html());
+// //console.log($('.te1').html());
+// //console.log($('#main_soccer').find('a');
+// //console.log($('#main_soccer').find('a').attr('href'));
+
+// //
 // $('#main_soccer td a').each(function(i, elem) {
 //   console.log($(this).text());
 //   console.log($(this).attr('href'));
 // });
 
- });
+//  });
