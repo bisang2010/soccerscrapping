ad772aed8aa1d6fb888ebb65ab569bcd67cec262
bisang2010 <bisang2010@gmail.com>
2015-04-24 14:18:15 +0900

    parsing

--- main.js
+++ main.js
@@ -7,54 +7,43 @@ var request = require('request'); //https://github.com/request/request
 var iconv = require('iconv-lite');
 
 
-//
-var testHtml = fs.readFileSync('TargetList.html','utf8');
+//var testHtml = fs.readFileSync('TargetList.html','utf8');
 
 //console.log(testHtml);
 
-var url = 'http://www.soccerline.co.kr/slboard/list.php?code=soccerboard'
-
-
+var url = 'http://www.soccerline.co.kr/slboard/list.php?code=locker'
 var requestOptions  = { encoding: null, uri: url};
 
 request(requestOptions, function(error, response, body) {
-    var utf8String = iconv.decode(body, 'euc-kr');
-    //console.log(utf8String);
 
+  var utf8String = iconv.decode(body, 'euc-kr');
+  var res = /content1 = "<table.*<\/table>/ig;
+  var parsing = res.exec(utf8String);
 
-$ = cheerio.load(utf8String);
-//console.log($.html());
-//console.log($('.te1').html());
-//console.log($('#main_soccer').find('a');
-//console.log($('#main_soccer').find('a').attr('href'));
+  while(parsing) {
+    // console.log(parsing[0]);
 
-//
-$('#main_soccer td a').each(function(i, elem) {
-  console.log($(this).text());
-  console.log($(this).attr('href'));
-});
+    console.log("\n>>>>>>>>>>>>>결과<<<<<<<<<<<<<<<<<");
+    $ = cheerio.load(parsing[0]);
+    $('table td a').each(function(i, elem) {
+      console.log($(this).text());
+      console.log($(this).attr('href'));
+    });
 
+    var parsing = res.exec(utf8String);
+  }
 
-});
-
-
-
-//   request(url, function(err, resp, body) {
-//      if (err)   throw err;
-//     //var bodyWithCorrectEncoding = iconv.decode(body, 'utf-8');
-//  // console.log(bodyWithCorrectEncoding);
-
-// $ = cheerio.load(body);
-// $ = cheerio.load(body.toString('utf8'));
+// $ = cheerio.load(utf8String);
 // //console.log($.html());
 // //console.log($('.te1').html());
 // //console.log($('#main_soccer').find('a');
 // //console.log($('#main_soccer').find('a').attr('href'));
-
+//
 // //
 // $('#main_soccer td a').each(function(i, elem) {
 //   console.log($(this).text());
 //   console.log($(this).attr('href'));
 // });
 
-//  });
+
+});
